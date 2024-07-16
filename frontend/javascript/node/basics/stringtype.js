2e0483b6c276359f242fe8626aad086b4df21498
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-16 19:32:28 +0530

    add strings

---
+++ frontend/javascript/node/basics/stringtype.js
@@ -0,0 +1,23 @@
+/* 3 ways of declaring strings
+    1. using double quotes ""
+    2. using the single quotes ''
+    3. using the back ticks (template literals)
+    strings are textual data
+*/
+
+// double quotes
+let city = "Singapore";
+
+// single quotes
+let name = "Alex";
+
+// back ticks
+let dept = `sales`;
+
+//  string  concatenation (combining strings) using the '+' operator
+
+let firstName = "David";
+let lastName = "Boon";
+let fullName = firstName + lastName;
+
+console.log("The full name is : " + fullName);
