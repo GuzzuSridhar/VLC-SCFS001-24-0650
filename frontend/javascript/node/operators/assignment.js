c3af5da50853c075e906a20eb0f33ca047761d33
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-20 11:11:25 +0530

    add assignments

---
+++ frontend/javascript/node/operators/assignment.js
@@ -0,0 +1,36 @@
+/* Assignment operators */
+
+// equals operator is used to assign a value to a variable
+let name = "David";
+let num = 10;
+
+/* other assignment operators */
+/* 
+    1. plus equals (can be used for both number and string types)
+    2. subtract equals
+    3. multiply equals
+    4. divide equals
+    5. modulo equals
+*/
+
+// plus equals
+num += 5; // same as writing num = num +5
+console.log(num); // 15
+name += " is my name";
+console.log(name); // David is my name
+
+//subtract plus
+num -= 5; // same as writing num = num -5
+console.log(num); // 10
+
+//multiply equals
+num *= 5; // same as writing num = num * 5
+console.log(num); // 50
+
+// divide equals
+num /= 5; // same as writing num = num / 5
+console.log(num); // 10
+
+// modulo equals
+num %= 10; // same as writing num = num / 10
+console.log(num); // 0
