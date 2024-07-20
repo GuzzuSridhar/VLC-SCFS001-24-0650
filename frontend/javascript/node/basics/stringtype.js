69b9509f7331310d30460689e404a75f2b16436b
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-20 06:50:13 +0530

    add strings

--- frontend/javascript/node/basics/stringtype.js
+++ frontend/javascript/node/basics/stringtype.js
@@ -7,9 +7,11 @@
 
 // double quotes
 let city = "Singapore";
+city = 'this is a "';
 
 // single quotes
 let name = "Alex";
+name = " this Alex's score ";
 
 // back ticks
 let dept = `sales`;
@@ -21,3 +23,14 @@ let lastName = "Boon";
 let fullName = firstName + lastName;
 
 console.log("The full name is : " + fullName);
+
+console.log("The first name is " + firstName + " and last name is " + lastName);
+// doing the same with back ticks
+console.log(`The first name is ${firstName} and last name is ${lastName}`);
+
+// back ticks preserve white spaces
+let wspace = " this is a string in multiple lines"; // cannot span on new line
+wspace = ` this 
+            is a string in 
+            multiple lines`;
+console.log(wspace);
