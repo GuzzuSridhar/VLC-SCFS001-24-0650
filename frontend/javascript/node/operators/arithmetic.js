100be0dfa4f9e21e06c7682aca2568dce8f05f0c
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-20 09:34:00 +0530

    add arithmetic operators

---
+++ frontend/javascript/node/operators/arithmetic.js
@@ -0,0 +1,54 @@
+/* 
+    Arithmetic operators
+    1. Add
+    2. Subtract
+    3. Multiply
+    4. Division
+    5. Modulo
+    6. increment (Post and pre)
+    7. decrement (Post and pre)
+*/
+
+let n1 = 10,
+  n2 = 20,
+  res;
+
+// add operator
+res = n1 + n2;
+console.log(res);
+
+// subtract operator
+res = n1 - n2;
+console.log(res);
+
+// multiply operator
+res = n1 * n2;
+console.log(res);
+
+// divide operator
+res = n1 / n2;
+console.log(res);
+
+// Modulo operator
+res = n1 % n2;
+console.log(res);
+
+console.log("--------------");
+// Increment post
+res = n1++;
+console.log(res);
+console.log(n1);
+
+// Increment pre
+res = ++n1;
+console.log(res);
+
+console.log("--------------");
+// decrement post
+res = n1--;
+console.log(res);
+console.log(n1);
+
+// decrement pre
+res = --n1;
+console.log(res);
