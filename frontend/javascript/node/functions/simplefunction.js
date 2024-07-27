ee6a5fe80023efc256564a591599a0f741ffa3f7
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-27 10:18:29 +0530

    add functions

---
+++ frontend/javascript/node/functions/simplefunction.js
@@ -0,0 +1,10 @@
+// a function is a set of js statements
+// not invoked implecitly
+function greet() {
+  console.log("Hello World");
+  console.log("Hello World again");
+  console.log("Hello World again and again");
+}
+
+// call a function
+greet();
