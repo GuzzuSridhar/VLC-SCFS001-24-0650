17823813420a510b5c7284470c39c3ca13717bc9
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-07-30 18:51:10 +0530

    edit object

--- frontend/javascript/ES6/destructuring/object.js
+++ frontend/javascript/ES6/destructuring/object.js
@@ -2,6 +2,7 @@ const book = {
   id: 25,
   title: "Java",
   author: "dave",
+  greet: () => console.log("hello"),
 };
 
 //  traditional way of accessing the prperties/feilds
@@ -11,6 +12,7 @@ console.log(book.title);
 
 console.log("-----------using the destructuring------------");
 
-const { title, author } = book;
+const { title, author, greet } = book;
 console.log(title);
 console.log(author);
+greet();
