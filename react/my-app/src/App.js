d3c2f73fdd9be59264717c766e30d222cca858e6
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-03 07:45:29 +0530

    add my-app

---
+++ react/my-app/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <h1> Welcome to React</h1>
+      <h2>something</h2>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit
+        temporibus dolores, molestias officiis aliquid, assumenda ad laboriosam
+        quisquam nostrum iure itaque eveniet expedita sit numquam sapiente sunt
+        sequi pariatur natus. <span>test</span>
+      </p>
+    </>
+  );
+}
+
+export default App;
