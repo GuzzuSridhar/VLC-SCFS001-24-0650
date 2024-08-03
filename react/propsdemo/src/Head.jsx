f5e101431da7af01b7d646c79ca5b6d2340c977e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-03 10:36:47 +0530

    added props destructure

--- react/propsdemo/src/Head.jsx
+++ react/propsdemo/src/Head.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Head(props) {
+    const { name, event } = props
     return (
         <div>
-            <p>Welcome!! {props.name} to {props.event} </p>
+            {/* <p>Welcome!! {props.name} to {props.event} </p> */}
+            <p>Welcome!! {name} to {event} </p>
         </div>
     )
 }
