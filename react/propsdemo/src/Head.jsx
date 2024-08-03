c5aff10176b8d34c10996b59919da3954c277870
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-03 10:26:15 +0530

    add props

---
+++ react/propsdemo/src/Head.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Head(props) {
+    return (
+        <div>
+            <p>Welcome!! {props.name} to {props.event} </p>
+        </div>
+    )
+}
+
+export default Head
