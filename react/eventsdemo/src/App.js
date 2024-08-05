5a0242a325c2e1e2b52bb5c24565f0cd8a0aef08
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-05 19:09:07 +0530

    add event object

--- react/eventsdemo/src/App.js
+++ react/eventsdemo/src/App.js
@@ -1,9 +1,12 @@
 import ButtonClick from "./ButtonClick";
+import EventDetails from "./EventDetails";
 
 function App() {
   return (
     <div>
       <ButtonClick />
+      <hr />
+      <EventDetails />
     </div>
   );
 }
---
+++ react/eventsdemo/src/EventDetails.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const EventDetails = () => {
+
+    const getEvent = (e) => {
+        console.log(e);
+        console.log(e.target);
+        console.log(e.target.tagName);
+        console.log(e.target.innerText);
+        console.log(e.target.value);
+
+        if (e.target.tagName === 'INPUT')
+            e.target.value = e.target.tagName + " " + e.type + " Event fired"
+        else
+            e.target.innerText = e.target.tagName + " " + e.type + " Event fired"
+    }
+
+    return (
+        <div>
+            <h1>Event Object Demo</h1>
+            <button onClick={getEvent}>Click</button>
+            <p onClick={getEvent}>Test</p>
+            <input type="text" onChange={getEvent} />
+        </div>
+    )
+}
+
+export default EventDetails
