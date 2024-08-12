9c86069059bfa12365c6a78779ec9d84f5ba9c7e
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-12 16:58:31 +0530

    add lazy load

--- react/multipage/src/App.js
+++ react/multipage/src/App.js
@@ -1,20 +1,31 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import HomePage from "./components/HomePage";
-import Contact from "./components/Contact";
-import AboutUs from "./components/AboutUs";
+// import HomePage from "./components/HomePage";
+// import Contact from "./components/Contact";
+// import AboutUs from "./components/AboutUs";
+
+// Not Loading the components on the page load (initial load),
+// these components will be imported on the first call
+// this will reduce the initial page load time
+const HomePage = lazy(() => import("./components/HomePage"));
+const Contact = lazy(() => import("./components/Contact"));
+const AboutUs = lazy(() => import("./components/AboutUs"));
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        {/* navigate to is used to link the end point to another path */}
+      {/* Suspense is used to load the component on the first use and the fa */}
+      <Suspense fallback={<div> Loading...........</div>}>
+        <Routes>
+          {/* navigate to is used to link the end point to another path */}
 
-        {/* the "/" end point is also referred to as root or default route */}
-        <Route path="/" element={<Navigate to="/home" />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/about" element={<AboutUs />} />
-      </Routes>
+          {/* the "/" end point is also referred to as root or default route */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 }
