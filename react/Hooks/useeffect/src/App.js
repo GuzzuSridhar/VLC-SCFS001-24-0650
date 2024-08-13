921e588f92269cadd0999336cff9ca078cdf6543
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-13 17:08:56 +0530

    add useEffect

---
+++ react/Hooks/useeffect/src/App.js
@@ -0,0 +1,11 @@
+import CallingParam from "./components/EffectParams";
+
+function App() {
+  return (
+    <div className="App">
+      <CallingParam />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/Hooks/useeffect/src/components/EffectParams.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+
+function EffectParams({ value }) {
+    // the use effect hook will get fired based on the second parameter
+
+
+    useEffect(() => {
+        console.log("UseEffect Fired")
+    })  // there is no second parameter, the useEffect would get firred on all renders
+    //}, []) // the second parameter is an empty dependency array, and would fire only on the initial mount
+    //}, [value])// the second parameter is a state and would fire on every value change of the state
+
+    return (
+        <div>
+            <h1>Use Effect Demo </h1>
+        </div>
+    )
+}
+
+function CallingParam() {
+    const [value, setValue] = useState(0)
+    return (
+        <div>
+            <EffectParams value={value} />
+            <button onClick={() => setValue(value + 1)}>click</button>
+        </div>
+    )
+}
+
+export default CallingParam
+
+
