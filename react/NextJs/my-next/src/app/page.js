b2dfb69c94b29b6e6d399d37ad60750442b373fc
GuzzuSridhar <132375692+GuzzuSridhar@users.noreply.github.com>
2024-08-17 07:51:00 +0530

    add next app

---
+++ react/NextJs/my-next/src/app/page.js
@@ -0,0 +1,11 @@
+// import Image from "next/image";
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+      Welcome to Next Js
+      {console.log("Hello")}
+    </main>
+  );
+}
